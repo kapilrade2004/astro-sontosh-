2356cb61895273a909c7039dc56bf4ab370c2441
vasifytech <vasifyt1@gmail.com>
2026-05-21 20:03:30 +0530

    update meta tag

--- src/pages/Inquiry.tsx
+++ src/pages/Inquiry.tsx
@@ -1388,11 +1388,29 @@ if (form.service === "Astrology" || form.service === "Numerology") {
 
 const Inquiry = () => (
   <>
-    <Helmet>
-      <title>Inquiry - Ask Your Question | Astro Santosh Pandey</title>
-      <meta name="description" content="Send an inquiry to Astro Santosh Pandey. Get personalized guidance on astrology, numerology, vastu, and palmistry." />
-      <link rel="canonical" href="https://astrosantoshpandey.com/inquiry" />
-    </Helmet>
+  <Helmet>
+  <title>Inquiry - Ask Your Question | Astro Santosh Pandey</title>
+  <meta name="description" content="Send an inquiry to Astro Santosh Pandey. Get personalized guidance on astrology, numerology, vastu, and palmistry." />
+  <link rel="canonical" href="https://astrosantoshpandey.com/inquiry" />
+
+  {/* Meta Pixel Code */}
+  <script>{`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1619073566059521');
+    fbq('track', 'PageView');
+  `}</script>
+  <noscript>{`
+    <img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1619073566059521&ev=PageView&noscript=1" />
+  `}</noscript>
+</Helmet>
     <Layout>
       <section className="pt-28 md:pt-32 pb-6 md:pb-8 bg-gradient-hero relative overflow-hidden">
         <div className="container mx-auto px-4 relative z-10">
@@ -1499,3 +1517,4 @@ const Inquiry = () => (
 );
 
 export default Inquiry;
+
